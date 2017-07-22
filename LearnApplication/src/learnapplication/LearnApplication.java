e58b46814fa93e38ebab5769876d68f84f71c0d7
learnproject <learnproject@lav>
2017-07-22 05:06:24 +0200

    now upload (ingestion) working

--- LearnApplication/src/learnapplication/LearnApplication.java
+++ LearnApplication/src/learnapplication/LearnApplication.java
@@ -12,8 +12,13 @@ import java.net.*;
 import java.util.Scanner;
 import static javafx.scene.input.KeyCode.S;
 import static javax.swing.text.html.HTML.Tag.S;
+import learnapplication.Utilities.VulaApiUtils;
 import learnapplication.responses.User;
 import learnapplication.services.VulaAPIServices;
+import learnapplication.services.VulaService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import sun.misc.BASE64Encoder;
 
 /**
@@ -21,35 +26,57 @@ import sun.misc.BASE64Encoder;
  * @author nkateko
  */
 public class LearnApplication {
-
+    private static VulaService vService;
     public static void main(String[] args) throws Exception {
-        FedoraAPIService client = new FedoraAPIService();
-        
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter file name: ");
-        File snd_file = new File(input.nextLine());
-        System.out.println("File exists : " +snd_file.exists());
-        System.out.println("File can be Read : " +snd_file.canRead());
-        System.out.println("File can be written : " +snd_file.canRead());
-        String file_name = snd_file.getName();
-        String mime = URLConnection.guessContentTypeFromName(file_name);
-
-
-        System.out.println("mimeType : "+mime);
-        String pid = client.createObject("candy","lovedare", "Lavius Nkateko Motileng");
-        System.out.println("PID : "+pid);
-        
-        String location = client.uploadFile(client.BASE_URL + "upload", snd_file);
-        System.out.println(location);
-        
-        client.createNewDataStream(pid, "newobject", mime, "uploading first version of the file",file_name, location);
+//        FedoraAPIService client = new FedoraAPIService();
+//        
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter file name: ");
+//        File snd_file = new File(input.nextLine());
+//        System.out.println("File exists : " +snd_file.exists());
+//        System.out.println("File can be Read : " +snd_file.canRead());
+//        System.out.println("File can be written : " +snd_file.canRead());
+//        String file_name = snd_file.getName();
+//        String mime = URLConnection.guessContentTypeFromName(file_name);
+//
+//
+//        System.out.println("mimeType : "+mime);
+//        String pid = client.createObject("candy","lovedare", "Lavius Nkateko Motileng");
+//        System.out.println("PID : "+pid);
+//        
+//        String location = client.uploadFile(client.BASE_URL + "upload", snd_file);
+//        System.out.println(location);
+//        
+//        client.createNewDataStream(pid, "newobject", mime, "uploading first version of the file",file_name, location);
         
         //client.gfindObjects("learn", "pid~learn:*");
+
         
 //        Gson gson = new Gson();
 //        int[] ints = {1, 2, 3, 4, 5};
 //        String results = gson.toJson(new User()); 
 //        System.out.println(results);
           //new VulaAPIServices();
-    }
+    vService = VulaApiUtils.getVulaService();
+//    vService.getUserProfile().enqueue(new Callback<User>() {
+//    @Override
+//    public void onResponse(Call<User> call, Response<User> response) {
+// 
+//        if(response.isSuccessful()) {
+//            String username = response.body().getDisplayName();
+//            System.out.println("Got name : "+username);
+//        }else {
+//            int statusCode  = response.code();
+//            System.out.println("Error occured");            
+//        }
+//    }
+// 
+//    @Override
+//    public void onFailure(Call<User> call, Throwable t) {
+//        System.out.println("error loading from API");
+//        }
+//    });      
+          
+          
+}
 }
