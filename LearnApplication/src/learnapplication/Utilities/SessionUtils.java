de210a0a2e78f7b1784ec2cd7f485d2af090f844
learnproject <learnproject@lav>
2017-07-23 20:21:54 +0200

    Updating Creating Session Utility classes

--- LearnApplication/src/learnapplication/Utilities/SessionUtils.java
+++ LearnApplication/src/learnapplication/Utilities/SessionUtils.java
@@ -5,6 +5,8 @@
  */
 package learnapplication.Utilities;
 
+import learnapplication.responses.User;
+
 /**
  *
  * @author learnproject
@@ -13,7 +15,23 @@ public abstract class SessionUtils {
     
     protected String SESSION_ID;
     protected String SESSION_SUB_URL;
+    protected String BASE_URL;
+    private static User user;
+    
+    public SessionUtils(String SESSION_SUB_URL, String BASE_URL) {
+        this.SESSION_SUB_URL = SESSION_SUB_URL;
+        this.BASE_URL = BASE_URL;
+    }
     
     public abstract void create();
-    public abstract String getSessionID();    
+    public abstract String getSessionID();
+
+    public static void setUser(User user) {
+        SessionUtils.user = user;
+    }
+
+    public static User getUser() {
+        return user;
+    }  
+    
 }
--- LearnApplication/src/learnapplication/Utilities/VulaSessionUtils.java
+++ LearnApplication/src/learnapplication/Utilities/VulaSessionUtils.java
@@ -5,6 +5,8 @@
  */
 package learnapplication.Utilities;
 
+import learnapplication.responses.User;
+
 /**
  *
  * @author learnproject
@@ -12,7 +14,12 @@ package learnapplication.Utilities;
 public class VulaSessionUtils extends SessionUtils{
     private final String USERNAME_KEY = "_username";
     private final String PASSWORD_KEY = "_password";
-
+    
+    public VulaSessionUtils() {
+        super("/session", "https://vula.uct.ac.za/direct");
+    }
+    
+    
     @Override
     public void create() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
